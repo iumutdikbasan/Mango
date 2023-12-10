7d7d667a0d013b76733c23ebffc28a6a033434e0
iumutdikbasan <iumutdikbasan@gmail.com>
2023-12-10 16:18:20 +0300

    Add project files.

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private string connectionString =
+            "Endpoint=sb://mangomsweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Oyifp9BUf41JT8KGQcldKCOs2ElUDtnev+ASbCZin4A=";
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding
+                .UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+        
+    }
+}
---
+++ Mango.Services.EmailAPI/Models/Dto/CartDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.EmailAPI.Models.Dto;
+
+namespace Mango.Services.EmailAPI.Models
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeaderDto? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
